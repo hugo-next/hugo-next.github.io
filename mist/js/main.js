920179004c47d85b793a3263ec8dfd93690029e9
elkan1788 <elkan1788@users.noreply.github.com>
2023-04-07 06:36:39 +0000

    :lipstick: Update the theme develop change. hugo-next/hugo-next-docs@3a24b110ce3e42d890b2fffd52fa3ad4603b7519

--- mist/js/main.js
+++ mist/js/main.js
@@ -597,8 +597,11 @@ NexT.utils = {
     });
   },
 
-  hideCommontes: function () {
-    document.querySelector('.post-comments').style.display = 'none';
+  hideComments: function () {
+    let postComments = document.querySelector('.post-comments');
+    if (postComments !== null) {
+        postComments.style.display = 'none';
+    }
   },
 
   hiddeLodingCmp: function (selector) {
@@ -824,7 +827,7 @@ NexT.boot.refresh = function() {
     NexT.utils.registerCommonSwitch();
     NexT.utils.domAddClass('#goto-comments', 'goto-comments-on');
   } else {
-    NexT.utils.hideCommontes();
+    NexT.utils.hideComments();
   }
   NexT.utils.registerImageViewer();
 
@@ -1125,42 +1128,6 @@ NexT.plugins.comments.waline = function() {
       NexT.utils.hiddeLodingCmp(element);
     })
   });
-}
-;
-/* Page's view & comment counter plugin */
-NexT.plugins.others.counter = function() {
-    let busz_postview = false;
-    let pageview_js = undefined;
-    let comment_js = undefined;
-
-  const busz = NexT.CONFIG.busuanzi;
-  if (busz != undefined && busz.pageview) {
-    busz_postview = true;
-  }
-
-  // Here can append others pageview & comment plugin
-  const waline = NexT.CONFIG.waline;
-  if (waline != undefined){
-    if(!busz_postview && waline.cfg.pageview) {
-      pageview_js = NexT.utils.getCDNResource(NexT.CONFIG.page.waline.js[0]);
-      NexT.utils.getScript(pageview_js, function(){
-        Waline.pageviewCount({
-          serverURL: waline.cfg.serverurl
-        });
-      });
-    }
-
-    if (waline.cfg.comment) {
-      comment_js = NexT.utils.getCDNResource(NexT.CONFIG.page.waline.js[1]);
-      NexT.utils.getScript(comment_js, function(){
-        Waline.commentCount({
-          serverURL: waline.cfg.serverurl
-        });
-      });
-    }
-  }
-
- 
 }
 ;
 /* Giscus comment plugin */
