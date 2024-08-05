31fa6adaef2ed323427d186f7a461d6feda4a344
elkan1788 <elkan1788@users.noreply.github.com>
2024-08-05 12:54:22 +0000

    :sparkles: Add post share plugin. hugo-next/hugo-next-docs@20666b1742f02a96902abe4408c83b9e4f4366ff

--- mist/js/main.js
+++ mist/js/main.js
@@ -1101,6 +1101,31 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 ;
+/* Addtoany share plugin */
+NexT.plugins.share.addtoany = function() {
+  const element = '.a2a_kit';
+  if (!NexT.CONFIG.share.enable || !NexT.utils.checkDOMExist(element)) return; 
+
+  const addtoany = NexT.CONFIG.share.addtoany;
+
+  if (!addtoany) return;
+
+  NexT.utils.lazyLoadComponent(element, function() {
+    let addtoany_cfg = `
+      var a2a_config = a2a_config || {};
+      a2a_config.onclick = 1;
+      a2a_config.locale = "${addtoany.locale}";
+      a2a_config.num_services = ${addtoany.num};
+    `;
+
+    NexT.utils.getScript(null, {
+      textContent: addtoany_cfg
+    });   
+    
+    NexT.utils.getScript(addtoany.js, () => { NexT.utils.hiddeLodingCmp(element); });
+  });
+}
+;
 /* Waline3 comment plugin */
 NexT.plugins.comments.waline3 = function () {
   const element = '.waline3-container';
